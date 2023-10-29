b3c352f450c6dbc587bf729fa4c4b44f52539e08
Tharindu_Chathuranga <it20168872@my.sliit.lk>
2023-10-29 14:59:17 +0530

    1

--- app.py
+++ app.py
@@ -91,4 +91,4 @@ def predict():
 
 # Start the Flask app if this script is executed
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=3000, debug=True)
